aa04889eac344a54d46f669bc2c632c575036597
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2021-05-17 12:19:30 +0100

    implement tax calulator for petrol vehicle in first year

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -13,8 +13,26 @@ let TaxCalculator = class TaxCalculator {
   }
 
   calculateTax(vehicle) {
-    throw new Error('You have to implement the method doSomething!');
-  }
-}
+    const emissions = vehicle.co2Emissions;
+    console.log(emissions);
+    let price;
 
-module.exports = { TaxCalculator: TaxCalculator }
+    if (emissions < 1) price = 0;
+    else if (emissions < 51) price = 10;
+    else if (emissions < 76) price = 25;
+    else if (emissions < 91) price = 105;
+    else if (emissions < 101) price = 125;
+    else if (emissions < 111) price = 145;
+    else if (emissions < 131) price = 165;
+    else if (emissions < 151) price = 205;
+    else if (emissions < 171) price = 515;
+    else if (emissions < 191) price = 830;
+    else if (emissions < 226) price = 1240;
+    else if (emissions < 256) price = 1760;
+    else price = 2070;
+
+    return price;
+  }
+};
+
+module.exports = { TaxCalculator: TaxCalculator };
