2f9a34f1d61f29a3d77ffef7c5b4e540639b71e7
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-05-17 15:49:39 +0100

    story-two

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -12,9 +12,34 @@ let TaxCalculator = class TaxCalculator {
     return this.year;
   }
 
-  calculateTax(vehicle) {
-    return this.calculatePetroltax(vehicle.co2Emissions);
+  calculateTax({ fuelType, co2Emissions}){
+   if(fuelType === fuelType.PETROL){
+      return this.calculatePetroltax(co2Emissions)
+   }else{
+    return this.calculateDieselTax(co2Emissions)
+ }
+                
   }
+
+  calculateDieselTax(emissions) {
+    let price;
+
+    if (emissions < 1) price = 0;
+    else if (emissions < 51) price = 25;
+    else if (emissions < 76) price = 105;
+    else if (emissions < 91) price = 125;
+    else if (emissions < 101) price = 145;
+    else if (emissions < 111) price = 165;
+    else if (emissions < 131) price = 205;
+    else if (emissions < 151) price = 515;
+    else if (emissions < 171) price = 830;
+    else if (emissions < 191) price = 1240;
+    else if (emissions < 226) price = 1760;
+    else if (emissions < 256) price = 2070;
+    else price = 2070;
+
+    return price;
+    }
 
   calculatePetroltax(emissions) {
     let price;
